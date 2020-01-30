758fbdffe55c97e9ecd410df33a7b4fd7ce51490
Ryan Jamison <ryanjamison26@gmail.com>
2020-01-30 15:35:43 -0500

    first commit todo components complete, toggle functionality, and props

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos'
+
+import "./App.css";
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id:1,
+        title: ' Take out the trash',
+        completted: false
+      },
+
+      {
+        id:2,
+        title: ' Dinner with wife',
+        completed: false
+      },
+
+      {
+        id:3,
+        title: ' Meeting with boss',
+        completted: false
+      },
+    ]
+  }
+
+  //Toggel Complete
+  markComplete = (id) => {
+       
+  this.setState({todos: this.state.todos.map(todo =>{
+    if(todo.id === id) {
+      todo.completed = !todo.completed
+    }
+    return todo;
+  }) });
+    }
+
+  render(){
+    console.log(this.state.todos)
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+      </div>
+    );
+  }
+  }
+  
+    
+
+export default App;
