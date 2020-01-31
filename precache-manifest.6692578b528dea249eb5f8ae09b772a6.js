5199c503d1d91cc379040328f3b344a13bb802e6
Ryan Jamison <ryanjamison26@gmail.com>
2020-01-31 18:25:10 -0500

    Updates

---
+++ precache-manifest.6692578b528dea249eb5f8ae09b772a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d5cf234b6ec1ba47dca897d9be3436a",
+    "url": "/react_todo_list/index.html"
+  },
+  {
+    "revision": "2e121ae3e8811a24d260",
+    "url": "/react_todo_list/static/css/main.2fdbd89a.chunk.css"
+  },
+  {
+    "revision": "269e23f8529c8f095351",
+    "url": "/react_todo_list/static/js/2.7f6d7a7f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react_todo_list/static/js/2.7f6d7a7f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e121ae3e8811a24d260",
+    "url": "/react_todo_list/static/js/main.237388e4.chunk.js"
+  },
+  {
+    "revision": "96acca18272628f8fe60",
+    "url": "/react_todo_list/static/js/runtime-main.a6a4ca38.js"
+  }
+]);
